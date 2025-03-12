0734ea242f5cd94a16e7d401a57b02384a18f79c
up240554 <up240554@alumnos.upa.edu.mx>
2025-03-12 09:46:08 -0600

    .

--- Dia6.py
+++ Dia6.py
@@ -35,3 +35,19 @@ print(food_stuff_tp)
 
 #3
 food_stuff_lt = list(food_stuff_tp)
+
+#4
+print(food_stuff_tp[7:8])
+
+#5
+first_three_items = food_stuff_tp[:3]
+last_three_items = food_stuff_tp[-3:]
+print("first_three_items", "last_three_items")
+
+#6
+del food_stuff_tp
+
+#7
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Norway' in nordic_countries)
+print('Denmark' in nordic_countries)
