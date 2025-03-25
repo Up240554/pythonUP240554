75d41e7b6bd2e08c609933ce175d400c8ac7cf3c
up240554 <up240554@alumnos.upa.edu.mx>
2025-03-25 09:21:23 -0600

    .

---
+++ 08_Day/Dia8.py
@@ -0,0 +1,29 @@
+#1
+dog = {}
+
+#2
+dog["name"] = "Paco"
+dog["color"] = "Negro"
+dog["breed"] = "Dobberman"
+dog["legs"] = 4
+dog["age"] = 1
+
+#3
+Alumnno= {"Nombre": "Diego",
+    "Apellido": "Lopez",
+    "Genero": "Masculino",
+    "Edad": "19",
+    "Estado civil": "Soletero",
+    "Habilidades":"Liderazgo",
+    "Pais": "Mexico",
+    "Ciudad": "Aguascalientes",
+    "Direccion": "de los Flamingos 107"}
+print("Alumno")
+
+#4
+print (len("Alumno"))
+
+#5
+print(list("Alumno"['Habilidades']))
+
+#6
