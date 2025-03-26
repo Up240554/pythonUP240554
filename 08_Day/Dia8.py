5c158ff72f5d75924339856b0c4eca2ec0e3fab8
up240554 <up240554@alumnos.upa.edu.mx>
2025-03-26 09:30:34 -0600

    .

--- 08_Day/Dia8.py
+++ 08_Day/Dia8.py
@@ -9,21 +9,43 @@ dog["legs"] = 4
 dog["age"] = 1
 
 #3
-Alumnno= {"Nombre": "Diego",
+Alumno= {"Nombre": "Diego",
     "Apellido": "Lopez",
     "Genero": "Masculino",
     "Edad": "19",
     "Estado civil": "Soletero",
-    "Habilidades":"Liderazgo",
+    "Habilidades":["Liderazgo",],
     "Pais": "Mexico",
     "Ciudad": "Aguascalientes",
     "Direccion": "de los Flamingos 107"}
 print("Alumno")
 
 #4
-print (len("Alumno"))
+print (len(Alumno))
 
 #5
-print(list("Alumno"['Habilidades']))
+print(list(Alumno['Habilidades']))
 
 #6
+Alumno["Habilidades"].extend(["Resolución de problemas"])
+print("Alumno")
+
+
+#7
+keys_list = list(Alumno.keys())
+print("Claves del diccionario Alumno:", keys_list)
+
+#8
+values_list = list(Alumno.values())
+print("Valores del diccionario Alumno:", values_list)
+
+#9
+Estudiantes= list(Alumno.items())
+print(Estudiantes)
+
+#10
+del Alumno ["Direccion"]
+print(Alumno)
+
+#11
+del dog
