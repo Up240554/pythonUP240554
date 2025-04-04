76221b6a3f8eb18283706ac38e8fd30e070a11a4
up240554 <up240554@alumnos.upa.edu.mx>
2025-04-03 23:52:20 -0600

    ,

---
+++ Trabajos de python/15_Day/Dia15.py
@@ -0,0 +1,55 @@
+# Exercises: Day 15
+#1#
+print 'hello world'  
+#solucion 1
+print('hello world')
+
+#2
+print(age)
+#solucion 2
+age = 25
+print(age)
+
+#3
+numbers = [1, 2, 3, 4, 5]
+print(numbers[5])
+#solucion 3
+print(numbers[4])
+
+#4
+ import maths 
+#solucion 4
+import math
+
+#5
+print(math.PI)
+#solucion 5
+print(math.pi)
+
+#6
+user = {'name': 'Asab', 'age': 250, 'country': 'Finland'}
+print(user['county'])
+#solucion 6
+print(user['country'])
+
+#7
+ 4 + '3'
+#solucion 7
+print(4 + int('3'))
+
+#8
+from math import pow
+#solucion 8
+from math import pow
+print(pow(2, 3))
+
+#9
+int('12a') 
+#solucion 9
+print(int('12'))
+
+#10
+
+1 / 0 
+#solucion 10
+print(1 / 1)
